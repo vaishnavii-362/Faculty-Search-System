3906c31ec3199ea5b7be164dc37ff1fa554dc101
vaishnavii-362 <vaishnavi.25bce10245@vitbhopal.ac.in>
2025-11-24 20:10:27 +0530

    Add files via upload

---
+++ faculty_info.py
@@ -0,0 +1,50 @@
+faculty_list = [
+    {
+        "name": "Sasmita Padhy",
+        "course": "Introduction to problem solving and programming",
+        "slot": "C14+E11+E12",
+        "email": "sasmitapadhy@vitbhopal.ac.in"
+    },
+    {
+        "name": "G. Prabu Kanna",
+        "course": "Introduction to problem solving and programming",
+        "slot": "C11+C12+C13",
+        "email": "gprabhukannan@vitbhopal.ac.in"
+    },
+    {
+        "name": "S. Periyanayagi",
+        "course": "Introduction to problem solving and programming",
+        "slot": "A14+D11+D12",
+        "email": "speriyanayagi@vitbhopal.ac.in"
+    },
+    {
+        "name": "Dheresh Soni",
+        "course": "Introduction to problem solving and programming",
+        "slot": "A14+D11+D12",
+        "email": "dheereshsoni@vitbhopal.ac.in"
+    },
+    {
+        "name": "A.V.R Mayuri",
+        "course": "Introduction to problem solving and programming",
+        "slot": "A11+A12+A13",
+        "email": "avrmayuri@vitbhopal.ac.in"
+    },
+    {
+        "name": "J. Sharmila Joseph",
+        "course": "Introduction to problem solving and programming",
+        "slot": "A14+D11+D12",
+        "email": "dheereshsoni@vitbhopal.ac.in"
+    },
+    {
+        "name": "Poonkuntran S",
+        "course": "Introduction to problem solving and programming",
+        "slot": "A21+A22+A23",
+        "email": "poonkuntrans@vitbhopal.ac.in"
+    },
+    {
+        "name": "D. Lakshmi",
+        "course": "Introduction to problem solving and programming",
+        "slot": "A21+A22+A23",
+        "email": "dlakshmi@vitbhopal.ac.in"
+    },
+]
